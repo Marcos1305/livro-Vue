61c987f608577d5933d85f28bcaedd63428c51df
Marcos Vinicius <marcos1305pt@gmail.com>
2018-11-14 14:08:05 -0200

    Capitulo Components

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <the-content></the-content>
+    <the-footer></the-footer>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader';
+import TheFooter from '@/components/TheFooter';
+import TheContent from '@/components/TheContent';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    TheHeader,
+    TheFooter,
+    TheContent,
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+  created() {
+    this.$materialize.toast('Its Happening', 1000);
+  },
+};
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import materialize from 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+Vue.prototype.$materialize = materialize;
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});
